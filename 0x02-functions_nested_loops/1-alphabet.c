a089a9dbff8ae4daff726a15881bc542031136f8
josiah <josiahomwancha@gmail.com>
2023-03-21 12:20:33 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_alphabet - prints lower case letters
+ */
+void print_alphabet(void)
+{
+	char la;
+	for (la='a' ; la<= 'z' ; la++)
+		_putchar (la);
+	_putchar('\n');
+}
