2851b03102cc78a6be532b4e924004cca3177333
josiah <josiahomwancha@gmail.com>
2023-03-21 12:03:09 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,10 @@
 #include "main.h"
-
 /**
  * main - Entry point
  * Return: Always 0
  */
-
 int main(void)
-
 {
-
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -19,5 +15,4 @@ int main(void)
 	_putchar('r');
 	_putchar('\n');
 	return (0);
-
 }      
