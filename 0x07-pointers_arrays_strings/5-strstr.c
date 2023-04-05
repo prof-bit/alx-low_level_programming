60bff6c92ad99e0e92afd7d767c489980f5354b0
josiah <josiahomwancha@gmail.com>
2023-04-04 19:41:59 -0700

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-#include <stddef.h>
 
 /**
  * *_strstr - locates a substring
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -8,3 +8,4 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 int _putchar(char c);
 void set_string(char **s, char *to);
+char *_strstr(char *haystack, char *needle);
